b69a73752bbfccde636bb54f3c4cf06cbc64cf89
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-23 21:41:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function takeANumber(katzDeliLine, name) {
+  console.log(katzDeliLine.push(name))
+  var place= katzDeliLine.length
+  return "Welcome, "+name+". You are number "+place+" in line."
+}
+
+function nowServing(katzDeliLine) {
+  if( katzDeliLine.length >0) {  
+    return `Currently serving ${katzDeliLine.shift()}.`
+ 
+  } else{
+    return "There is nobody waiting to be served!"
+  }
+}
